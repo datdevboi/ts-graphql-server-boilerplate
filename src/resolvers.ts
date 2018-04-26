a4ca809de730c5f5e5713f96a6bbad6057c485d0
camilo17 <cami172013@gmail.com>
2018-04-26 15:18:08 -0500

    Fixed some  errors with the importschema

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,13 @@
-export const resolvers = {
+import { ResolverMap } from "./types/graphql-utils";
+
+export const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
+    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) =>
+      `Hello ${name || "World"}`
+  },
+  Mutation: {
+    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+      return email + password;
+    }
   }
 };
