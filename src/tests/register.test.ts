a0e8b7a065c596c05566ca6f4b39ccab4dc5b1be
camilo17 <cami172013@gmail.com>
2018-04-27 15:12:02 -0500

    Added a few more test

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,3 +1,5 @@
+import { User } from "./../entity/User";
+import { createConnection } from "typeorm";
 import { host } from "./constants";
 import { request } from "graphql-request";
 
@@ -13,4 +15,11 @@ const mutation = `
 test("Register User", async () => {
   const response = await request(host, mutation);
   expect(response).toEqual({ register: true });
+  await createConnection();
+
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toBe(email);
+  expect(user.password).not.toEqual(password);
 });
