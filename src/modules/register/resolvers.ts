38e8f9a39a3bf5c9a766126067d67ce21dd227da
camilo17 <cami172013@gmail.com>
2018-05-02 12:46:58 -0500

    Added yup to test our schemas

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,13 +1,33 @@
 import * as bcrypt from "bcryptjs";
+import * as yup from "yup";
 import { ResolverMap } from "../../types/graphql-utils";
 import { User } from "../../entity/User";
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3)
+    .max(255)
+    .email(),
+  password: yup
+    .string()
+    .min(3)
+    .max(255)
+});
+
 export const resolvers: ResolverMap = {
   Mutation: {
-    register: async (
-      _,
-      { email, password }: GQL.IRegisterOnMutationArguments
-    ) => {
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      try {
+        await schema.validate(args, {
+          abortEarly: false
+        });
+      } catch (err) {
+        console.log(err);
+      }
+
+      const { email, password } = args;
+
       const userAlreadyExist = await User.findOne({
         where: {
           email
