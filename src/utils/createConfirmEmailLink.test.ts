9a9ee0b83f49d3a02a3e174bddb3d47a45616257
camilo17 <cami172013@gmail.com>
2018-05-05 21:22:56 -0500

    Created some test some more EmailLink test and the Redis removing key.

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -5,23 +5,45 @@ import { User } from "./../entity/User";
 import { createConfirmEmailLink } from "./createConfirmedEmailLink";
 
 let user: User;
+const redis: Redis.Redis = new Redis();
+let id: string = "";
 
 beforeAll(async () => {
   await createTypeormConn();
   user = await User.create({
-    email: "test@test.com",
-    password: "test@test"
+    email: "test2@test.com",
+    password: "test2@test"
   }).save();
 });
-test("Make sure createConfirmEmailLink", async () => {
-  const url = await createConfirmEmailLink(
-    process.env.TEST_HOST as string,
-    user.id as string,
-    new Redis()
-  );
 
-  const response = await fetch(url);
-  const text = await response.text();
+describe("test createConfirmEmailLink", async () => {
+  test("Make sure it confirms user and clears key in redis", async () => {
+    const url = await createConfirmEmailLink(
+      process.env.TEST_HOST as string,
+      user.id,
+      redis
+    );
 
-  expect(text).toBe("ok");
+    const response = await fetch(url);
+    const text = await response.text();
+
+    expect(text).toBe("ok");
+    await user.reload();
+    expect(user.confirmed).toBeTruthy();
+
+    const urlArray = url.split("/");
+    id = urlArray[urlArray.length - 1];
+
+    const data = await redis.get(id);
+    expect(data).toBeNull();
+  });
+
+  test("sends invalid back if bad id is used", async () => {
+    const response = await fetch(
+      `${process.env.TEST_HOST}/confirm/ajfkdjgijdirhakfj`
+    );
+    const text = await response.text();
+
+    expect(text).toBe("invalid");
+  });
 });
