59d6c9cd892c42d6ebd5ef662d70746e3d4c1910
camilo17 <cami172013@gmail.com>
2018-05-07 19:40:59 -0500

    created a function to create Schema and used it for our server

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,32 +1,14 @@
+import { genSchema } from "./utils/genSchema";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
-import { importSchema } from "graphql-import";
 
 import { createTypeormConn } from "./utils/createTypeormConn";
-import * as fs from "fs";
-import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
-import { GraphQLSchema } from "graphql";
 
 import { confirmEmail } from "./routes/confirmEmail";
 import { redis } from "./redis";
 
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
-
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: genSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host")
---
+++ src/utils/genSchema.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+import * as path from "path";
+import { importSchema } from "graphql-import";
+
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+
+export const genSchema = () => {
+  const schemas: GraphQLSchema[] = [];
+
+  const folders = fs.readdirSync(path.join(__dirname, "../modules"));
+
+  folders.forEach(folder => {
+    const { resolvers } = require(`../modules/${folder}/resolvers`);
+
+    const typeDefs = importSchema(
+      path.join(__dirname, `../modules/${folder}/schema.graphql`)
+    );
+
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  return mergeSchemas({ schemas });
+};
