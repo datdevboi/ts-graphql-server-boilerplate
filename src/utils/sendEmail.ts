9b6ec75d933ee52e58738e408d25d5b0485a8cef
camilo17 <cami172013@gmail.com>
2018-05-09 18:55:26 -0500

    Added sparkpost and created a sendEmail function

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,21 @@
+import * as Sparkpost from "sparkpost";
+
+const client = new Sparkpost(process.env.SPARKPOST_API_KEY);
+
+export default async (recipient: string, url: string) => {
+  const response = await client.transmissions.send({
+    options: {
+      sandbox: true
+    },
+    content: {
+      from: "testing@sparkpostbox.com",
+      subject: "Confirm Email",
+      html: `<html><body><p>Testing SparkPost - the world's most awesomest email service!</p>
+      <a href=${url}>Confirm Email</a>
+      </body></html>`
+    },
+    recipients: [{ address: recipient }]
+  });
+
+  console.log(response);
+};
