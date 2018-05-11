e5fcfe3f4af7c678707bb2dae76a049c597965c8
camilo17 <cami172013@gmail.com>
2018-05-11 18:18:42 -0500

    Made a Resolver type

--- src/types/graphql-utils.ts
+++ src/types/graphql-utils.ts
@@ -3,17 +3,20 @@ import { Redis } from "ioredis";
 export interface Session {
   userId?: string;
 }
+
+export type Resolver = (
+  parent: any,
+  args: any,
+  context: {
+    redis: Redis;
+    url: string;
+    session: Session;
+  },
+  info: any
+) => any;
+
 export interface ResolverMap {
   [key: string]: {
-    [key: string]: (
-      parent: any,
-      args: any,
-      context: {
-        redis: Redis;
-        url: string;
-        session: Session;
-      },
-      info: any
-    ) => any;
+    [key: string]: Resolver;
   };
 }
