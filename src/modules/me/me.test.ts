3c8f1862d6af0b46d0130e2f9e7ae2cdfb4fca4c
camilo17 <cami172013@gmail.com>
2018-05-17 19:44:58 -0500

    Switched the me test to use our TestClient

--- src/modules/me/me.test.ts
+++ src/modules/me/me.test.ts
@@ -1,3 +1,4 @@
+import { TestClient } from "./../../utils/TestClient";
 import { User } from "./../../entity/User";
 import axios from "axios";
 import { createTypeormConn } from "../../utils/createTypeormConn";
@@ -42,38 +43,22 @@ const meQuery = `
 `;
 describe("me", async () => {
   test("return null if no cookie", async () => {
+    const client = new TestClient(process.env.TEST_HOST as string);
     // User is not logged in
-    const response = await axios.post(process.env.TEST_HOST as string, {
-      query: meQuery
-    });
+    const response = await client.me();
 
-    expect(response.data.data.me).toBeNull();
+    expect(response.data.me).toBeNull();
   });
 
   test("get current user", async () => {
+    const client = new TestClient(process.env.TEST_HOST as string);
     // Login the user
-    await axios.post(
-      process.env.TEST_HOST as string,
-      {
-        query: loginMutation(email, password)
-      },
-      {
-        withCredentials: true
-      }
-    );
+    await client.login(email, password);
 
     // Get current User
-    const response = await axios.post(
-      process.env.TEST_HOST as string,
-      {
-        query: meQuery
-      },
-      {
-        withCredentials: true
-      }
-    );
+    const response = await client.me();
 
-    const currentUser = response.data.data.me;
+    const currentUser = response.data.me;
     expect(currentUser.email).toEqual(email);
     expect(currentUser.id).toEqual(userId);
   });
