a8c038db21bf9898a7da9bfd09fccb5ca72a5b89
camilo17 <cami172013@gmail.com>
2018-05-21 19:50:56 -0500

    made a simple sendForgotPasswordEmail

--- src/modules/forgotPassword/resolvers.ts
+++ src/modules/forgotPassword/resolvers.ts
@@ -1,8 +1,8 @@
-// import { userSessionIdPrefix } from "./../../constants";
-// import * as bcrypt from "bcryptjs";
-
 import { ResolverMap } from "../../types/graphql-utils";
-// import { User } from "../../entity/User";
+import { forgotPasswordLockAccount } from "../../utils/forgotPasswordLockAccount";
+import { User } from "../../entity/User";
+import { createForgotPasswordLink } from "../../utils/createForgotPasswordLink";
+import { userNotFoundError } from "./errorMessages";
 // import { invalidLogin, confirmEmailError } from "./errorMessages";
 
 // // TODO FIX THE GENERATE SCHEMA SCRIPT ERROR
@@ -11,15 +11,25 @@ export const resolvers: ResolverMap = {
     dummy2: () => "bye"
   },
   Mutation: {
-    sendForgotPasswordEmail: async (
-      _,
-      {  }: GQL.IRegisterOnMutationArguments,
-      {}
-    ) => null,
-    forgotPasswordChange: async (
-      _,
-      {  }: GQL.IRegisterOnMutationArguments,
-      {}
-    ) => null
+    sendForgotPasswordEmail: async (_, { email }, { redis }) => {
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return [
+          {
+            path: "email",
+            message: userNotFoundError
+          }
+        ];
+      }
+
+      // lock account
+      await forgotPasswordLockAccount(user.id, redis);
+      // @todo add frontend url
+      await createForgotPasswordLink("", user.id, redis);
+      // @todo send email with url
+
+      return true;
+    },
+    forgotPasswordChange: async (_, { newEmail, key }, { redis }) => null
   }
 };
