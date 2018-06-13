d7771db7ad49eb3026ca8cd6b8368718ff16e161
camilo17 <cami172013@gmail.com>
2018-06-12 20:42:48 -0500

    Fixed a type with our genSchema

--- src/utils/genSchema.ts
+++ src/utils/genSchema.ts
@@ -8,13 +8,12 @@ import * as glob from "glob";
 
 export const genSchema = () => {
   const pathToModules = path.join(__dirname, "../modules");
-
   const graphqlTypes = glob
     .sync(`${pathToModules}/**/*.graphql`)
     .map(x => fs.readFileSync(x, { encoding: "utf8" }));
 
   const resolvers = glob
-    .sync(`{pathToModules}/**/resolvers.?s`)
+    .sync(`${pathToModules}/**/resolvers.?s`)
     .map(resolver => require(resolver).resolvers);
 
   return makeExecutableSchema({
